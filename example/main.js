1f7bdb87e1ae559aeb448f10b92738dcee910435
giorgi94 <giorgik1994@gmail.com>
2019-09-08 21:46:27 +0400

    added responsive example

--- example/main.js
+++ example/main.js
@@ -1,10 +1,12 @@
 /* globals SVGEditor SVGResponsivePath */
 
+const root = document.querySelector(".drawing svg");
+
 let points = [{ "x": 40, "y": 148 }, { "control": 2, "points": [{ "x": 119, "y": 14 }, { "x": 404, "y": 370 }] }, { "x": 656, "y": 254 }, { "control": 2, "points": [{ "x": 940, "y": 125 }, { "x": 745, "y": 443 }] }, { "x": 1084, "y": 284 }, { "control": 2, "points": [{ "x": 1272, "y": 195 }, { "x": 1500, "y": 326 }] }, { "x": 1444, "y": 470 }];
 
-// SVGEditor.root = document.querySelector(".drawing svg");
-// SVGEditor.addPath({ points, attrs: { id: "my path" } });
 
+// SVGEditor.root = root;
+// SVGEditor.addPath({ points, attrs: { id: "my path" } });
 // const path = SVGEditor.paths[0];
 
 const breakpoints = [
@@ -14,4 +16,9 @@ const breakpoints = [
     [863, ["M", [40, 148], "C", [119, 14], [200, 374], [437, 188], "C", [574, 82], [557, 374], [723, 212], "C", [827, 112], [756, 396], [844, 427]]]
 ];
 
-SVGResponsivePath.root = document.querySelector(".drawing svg");
+// SVGResponsivePath({
+//     root,
+//     element: document.querySelector("#mypath"),
+//     breakpoints,
+//     bounded: true
+// });
--- src/svg.responsive.js
+++ src/svg.responsive.js
@@ -1,31 +1,66 @@
-const SVGResponsivePath = function ({ root, element, breakpoints }) {
+const SVGResponsivePath = function ({ root, element, breakpoints, bounded = true }) {
+
+    function setd(p) {
+        element.setAttribute("d",
+            p.map(e => Array.isArray(e) ? e.join(",") : e).join(" "));
+    }
+
+    function getd() {
+        const d = element.getAttribute("d");
+
+        return d.split(" ")
+            .map(e => (e.indexOf(",") !== -1 ?
+                e.split(",").map(i => parseFloat(i)) : e));
+    }
+
+    function onresize() {
+        const rootWidth = root.getBoundingClientRect().width;
+        const bps = breakpoints.map(b => b[0]);
+
+        const index = bps.findIndex(b => b < rootWidth);
 
 
-    class Path {
-        constructor({ root }) {
-            this.element = null;
-            this.root = root;
+        if (index === 0) {
+            return setd(breakpoints[index][1]);
         }
 
-        get d() {
-            const d = this.element.getAttribute("d");
-
-            return d.split(" ")
-                .map(e => (e.indexOf(",") !== -1 ?
-                    e.split(",").map(i => parseFloat(i)) : e));
+        if (index === -1) {
+            return setd(breakpoints[bps.length - 1][1]);
         }
 
-        set d(p) {
-            this.element.setAttribute("d",
-                p.map(e => Array.isArray(e) ? e.join(",") : e).join(" "));
-        }
+        const [w1, p1] = breakpoints[index];
+        const [w2, p2] = breakpoints[index - 1];
+
+        const t = (rootWidth - w1) / (w2 - w1);
+
+
+        const p = p1.map((a, i) => {
+            if (!Array.isArray(a)) {
+                return a;
+            }
+
+            const b = p2[i];
+
+            return [
+                t * b[0] + (1 - t) * a[0],
+                t * b[1] + (1 - t) * a[1],
+            ].map(i => parseInt(i));
+        });
+
+
+        setd(p);
+    }
+
+    function init() {
+
+
+        window.addEventListener("resize", onresize);
+
+        onresize();
     }
 
 
-    return {
-        root,
-        element,
-        breakpoints,
-        path: new Path(root)
-    };
+
+
+    init();
 };
